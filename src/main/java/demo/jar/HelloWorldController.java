3888d86d9cd718af8ce5ee43ef96d949d13a6bd2
wom987 <45610462+wom987@users.noreply.github.com>
2022-03-13 15:36:35 -0600

    first commit

---
+++ src/main/java/demo/jar/HelloWorldController.java
@@ -0,0 +1,12 @@
+package demo.jar;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "hello world";
+    }
+}
