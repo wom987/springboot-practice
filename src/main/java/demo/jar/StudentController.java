b8c7073c8c4a949ef96c5e6a8def5aee8d75ab99
wom987 <45610462+wom987@users.noreply.github.com>
2022-03-13 17:39:41 -0600

    path variables and param variables added

--- src/main/java/demo/jar/StudentController.java
+++ src/main/java/demo/jar/StudentController.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
     @GetMapping("/student")
-    public Student gStudent(){
+    public Student gStudent() {
         return new Student("juan", "perez");
     }
+
     @GetMapping("/students")
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         List<Student> students = new ArrayList<>();
         students.add(new Student("juan", "perez"));
         students.add(new Student("juan1", "perez"));
@@ -21,5 +24,16 @@ public class StudentController {
         students.add(new Student("juan3", "perez"));
         students.add(new Student("juan4", "perez"));
         return students;
+    }
+
+    @GetMapping("/student/{firstname}/{lastname}")
+    public Student studentPathVar(@PathVariable("firstname") String firstName,
+            @PathVariable("lastname") String lastName) {
+        return new Student(firstName, lastName);
+    }
+    @GetMapping("/student/query")
+    public Student studentReqParam(@RequestParam("firstname") String firstName,
+            @RequestParam("lastname") String lastName) {
+        return new Student(firstName, lastName);
     }
 }
